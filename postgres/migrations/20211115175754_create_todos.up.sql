993c3e3f7d8586ffc96acceb624e6f9fac5561b7
ftcRibeiro <felipe.coltec@gmail.com>
2021-11-15 15:23:07 -0300

    migrations

--- postgres/migrations/20211115175754_create_todos.up.sql
+++ postgres/migrations/20211115175754_create_todos.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE todos(
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
     description TEXT,
+    done BOOLEAN,
     user_id BIGSERIAL REFERENCES users(id) ON DELETE CASCADE NOT NULL
 );
